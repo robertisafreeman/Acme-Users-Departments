5903bc7242bcda0e063eb29e600215a28903051d
Robert Freeman <robertisafreeman@gmail.com>
2019-09-14 11:07:46 -0700

    now we have bios

--- db.js
+++ db.js
@@ -26,16 +26,17 @@ const SQL = `
     CREATE TABLE users(
         id UUID PRIMARY KEY,
         name VARCHAR(255) UNIQUE NOT NULL, 
+        bio TEXT,
         departments_id UUID REFERENCES departments(id)
     );
     INSERT INTO departments(id, name) VALUES('${frontId}', 'Front Office');
     INSERT INTO departments(id, name) VALUES('${maintId}', 'Maintenance');
     INSERT INTO departments(id, name) VALUES('${houseId}', 'Housekeeping');
 
-    INSERT INTO users(id, name, departments_id) VALUES('${artId}', 'Arthur Sandro', '${frontId}');
-    INSERT INTO users(id, name, departments_id) VALUES('${natId}', 'Nathan Roy', '${maintId}');
-    INSERT INTO users(id, name, departments_id) VALUES('${robId}', 'Robert Freeman', '${houseId}');
-    INSERT INTO users(id, name) VALUES('${meowId}', 'Meow Mow');
+    INSERT INTO users(id, name, bio, departments_id) VALUES('${artId}', 'Arthur Sandro','Arthur likes sandwiches and pineapples', '${frontId}');
+    INSERT INTO users(id, name, bio, departments_id) VALUES('${natId}', 'Nathan Roy', 'Nathan really likes pickles and egyptians', '${maintId}');
+    INSERT INTO users(id, name, bio, departments_id) VALUES('${robId}', 'Robert Freeman', 'Robert is controlled by Meow Mow Zedong',  '${houseId}');
+    INSERT INTO users(id, name, bio) VALUES('${meowId}', 'Meow Mow', 'Chairman Meow Mow Zedong is an evil dictator');
 `;
 
 const syncAndSeed = async()=> {
